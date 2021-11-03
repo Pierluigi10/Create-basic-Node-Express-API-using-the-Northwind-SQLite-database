13095e8b734611ddbda5c0bda42d31a38ee2c98d
pierluigi <pierluigibaiano@gmail.com>
2021-11-03 14:00:18 +0100

    Create basic Node/Express API using the Northwind SQLite database

---
+++ backend/server.js
@@ -0,0 +1,24 @@
+import express from "express";
+import cors from "cors";
+import SqliteManager from "./sqliteManager.js";
+
+// const thePath = `${__dirname}/backend/data/assessmentData.sqlite`;
+const thePath = "./backend/data/northwind_database.sqlite";
+// console.log(thePath);
+
+const sqm = new SqliteManager(thePath);
+const records = await sqm.getRecordsWithSql(
+  `SELECT * FROM Products`
+); // default order ascending
+// console.log(records);
+
+const app = express();
+const port = process.env.PORT || 3011;
+
+app.use(cors());
+
+app.get("/", (req, res) => {
+  res.json(records);
+});
+
+app.listen(port, () => console.log(`Listening on http://localhost:${port}`));
