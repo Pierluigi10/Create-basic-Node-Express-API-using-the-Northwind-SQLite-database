2131c888b524e136a6bc1be5675bdd1cd3caf568
pierluigi <pierluigibaiano@gmail.com>
2021-11-03 15:14:40 +0100

    fetch the data from the / endpoint and display title and example records on the page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,39 @@
-import './App.scss';
+import "./App.scss";
+import { useState, useEffect } from "react";
 
 function App() {
+  const [records, setRecords] = useState([]);
+  const [title, setTitle] = useState();
+
+  useEffect(() => {
+    (async () => {
+      let response = await fetch("http://localhost:3044");
+      response = await response.json();
+      setRecords(response.exampleRecords);
+      setTitle(response.title);
+    })();
+  }, []);
+
+  // return (
+  //   <div className="App">
+  //     {records.map((record, index) => {
+  //       <div key={index}>
+  //         <p>{record.ProductID}</p>
+  //       </div>
+  //     })}
+  //   </div>
+  // );
+
   return (
     <div className="App">
-		Hello
+      <h1>{title}</h1>
+      {records.map((record, index) => (
+        <div key={index}>
+          <h2>{record.ProductID}</h2>
+          <p>{record.ProductName}</p>
+          <p>{record.UnitPrice}</p>
+        </div>
+      ))}
     </div>
   );
 }
